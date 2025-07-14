39b52e99a13e10798f79f8f1708399ead06d9627
winsonaccenture <winson.baring@accenture.com>
2025-07-14 16:22:11 +0800

    removed navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router'; // Import RouterOutlet and RouterLink
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterLink,RouterOutlet], // Make sure RouterLink is imported for the buttons
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
