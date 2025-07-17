136f437576cdede31b252350dd8bf324a2e2a233
winsonaccenture <winson.baring@accenture.com>
2025-07-17 08:16:33 +0800

    feat: add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { NgbdModalComponent } from '@/components/modal-component/modal-component.component';
 import { ProductService } from '@/shared/product.service';
+import { NavbarComponent } from '@/components/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule,
     RouterOutlet,
     NgbdModalComponent,
-    // RouterLink,
-    // RouterLinkActive,
+    NavbarComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,29 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FeaturesComponent } from '@/components/features/features.component';
+import { PricingComponent } from '@/components/pricing/pricing.component';
+import { SignInComponent } from '@/components/sign-in/sign-in.component';
+import { SignUpComponent } from '@/components/sign-up/sign-up.component';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'features',
+    component: FeaturesComponent,
+  },
+  {
+    path: 'pricing',
+    component: PricingComponent,
+  },
+  {
+    path: 'login',
+    component: SignInComponent,
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent,
+  },
 ];
