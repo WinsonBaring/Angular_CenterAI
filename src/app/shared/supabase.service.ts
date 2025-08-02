bbd45a5e3901fffedc80317f6da2989f2d815eed
Winson Baring <winsonbaring10@gmail.com>
2025-08-02 11:34:43 +0800

    update fix

--- src/app/shared/supabase.service.ts
+++ src/app/shared/supabase.service.ts
@@ -7,11 +7,10 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 })
 export class SupabaseService {
   public supabase: SupabaseClient;
-
   constructor() {
     this.supabase = createClient(
-      'https://vnlxtdubmdocbcboaydz.supabase.co',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZubHh0ZHVibWRvY2JjYm9heWR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0ODQ1ODIsImV4cCI6MjA2ODA2MDU4Mn0.CjFadQz7WWiCAbY5xyWLJ4iboky8iB9WfuLlBZs_Dlg'
+      'https://ydfujqyyedtussfgydfi.supabase.co',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkZnVqcXl5ZWR0dXNzZmd5ZGZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNzQzNzIsImV4cCI6MjA2NDY1MDM3Mn0.m6IxWw-pU1WmKTmp6jqk5m8UjNU2LvVI8bvd_Eezcmg'
       // environment.supabaseUrl,
       // environment.supabaseKey
     );
